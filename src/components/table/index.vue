058ed354ee397206874ef8d7d08061abe0e9feb7
“gaojikai” <1602405417@qq.com>
2022-11-20 12:01:49 +0800

    feat:table组件

---
+++ src/components/table/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column v-if="checkbox" type="selection" width="55"></el-table-column>
+    <el-table-column v-if="index" type="index" width="50"></el-table-column>
+    <el-table-column v-for="item in column" :key="item.prop" :prop="item.prop" :label="item.label" :width="item.width"></el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+  props: {
+    column: {
+      type: Array,
+      default: () => []
+    },
+    checkbox: Boolean,
+    index: Boolean
+    // checkbox: {
+    //   type: Boolean,
+    //   default: true
+    // },
+  },
+  data () {
+    return {
+      tableData: [
+        {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄',
+        sex:'男'
+        },
+        {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄',
+        sex: '女'
+        },
+        {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
+<style lang='less' scoped>
+</style>
---
+++ src/views/TableView.vue
@@ -0,0 +1,32 @@
+<template>
+ <a-table :column="column" checkbox index></a-table>
+</template>
+
+<script>
+// @ is an alias to /src
+//父组件 :checkbox="false"
+
+export default {
+  name: 'TableView',
+  components: {
+    'a-table': () => import("@/components/table/index.vue")
+  },
+  data() {
+    return {
+      column: [
+        { label: '日期', prop: 'date',width:100},
+        { label: '名称', prop: 'name' },
+        { label: '地址', prop: 'address'},
+        { label: '性别', prop: 'sex'},
+
+      ]
+    }
+  },
+  methods: {
+
+
+  }
+}
+</script>
+
+
